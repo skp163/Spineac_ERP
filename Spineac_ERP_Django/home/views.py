f6334700e334fc129add2a610a25df3ecc6f341a
skp163 <pradhansunilkumar163@yahoo.com>
2024-03-22 15:30:04 +0530

    Push

--- Spineac_ERP_Django/home/views.py
+++ Spineac_ERP_Django/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,"home/index.html")
+
+def success_page(request):
+    return HttpResponse("<h1> Hy its nice</h1>")
+
 # Create your views here.
